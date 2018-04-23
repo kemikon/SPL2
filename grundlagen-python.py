9e90e61a11cb1703a7965ea4031a33d2075b787f
kemikon <171wita23@o365.htl-leoben.at>
2018-04-23 14:09:32 +0200

    Python Programm1

--- grundlagen-python.py
+++ grundlagen-python.py
@@ -1,2 +1,18 @@
 # Grundlagen-python.py
 
+# Kommentare erfolgen mit hashtag
+
+# Ausgabe von Daten 
+print("Hallo World")
+
+# Variable definieren(kann ohne typ erfolgen)
+heimat = "Erde"
+
+# Mehrere Variable werden mit , getrennt
+print(heimat, "an World: ", "Hallo")
+
+#Eingabe/ liest Text von der Console ein
+wer  = input("Und wer bist du? ")
+
+# und gibt den Text wieder aus
+print("Hallo", wer) 
