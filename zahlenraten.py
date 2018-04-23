414f6ac9221b221d8552eddbc4714ab35b61f92b
kemikon <171wita23@o365.htl-leoben.at>
2018-04-23 16:19:39 +0200

    Nicht fertig

---
+++ zahlenraten.py
@@ -0,0 +1,45 @@
+# zahlenRaten.py
+import random
+print("Willkommen:Das Spiel heißt - ZahlenRaten(Computer spielt mit sich selbst")
+maxrange = input("Bitte MaxRange eingeben: ")
+gewonnen = False
+versuche = 1
+zufallszahl = random.randint(1,maxrange)
+print("Zufallszahl:", zufallszahl)
+while(gewonnen == False):
+    errattungDerZahl = maxrange/2
+    if(erratungDerZahl == zufallszahl):
+        gewonnen == True
+        print("Computer hat gewonnen.", "Versuche: ", versuche )
+    if(erratungDerZahl != zufallszahl):
+        print("Zahl von dem Computer: ", errattungDerZahl)
+        nichtErraten = input("Wenn die Zufallszahl ist > als die Zahl von dem computer, schreiben Sie 'nein', wenn die Zufallszahl ist < als die Zahl von dem Computer, schreiben Sie 'ja'")
+        versuche = int(versuche+1)
+    if(nichtErraten == "nein"):
+        errattungDerZahl = int(errattungDerZahl+(errattungDerZahl/2))
+        if(errattungDerZahl == zufallszahl):
+                gewonnen == True
+                print("Computer hat gewonnen.", "Versuche: ", versuche )
+        if(errattungDerZahl != zufallszahl):
+                print("Zahl von dem Computer: ", errattungDerZahl)
+                nichtErraten = input("Wenn die Zufallszahl ist > als die Zahl von dem computer, schreiben Sie 'nein', wenn die Zufallszahl ist < als die Zahl von dem Computer, schreiben Sie 'ja'")
+                versuche = int(versuche+1)
+                if(nichtErraten == "nein"):
+                    errattungDerZahl = int(errattungDerZahl+(errattungDerZahl/4))
+                    if(errattungDerZahl == zufallszahl):
+                        gewonnen == True
+                        print("Computer hat gewonnen.", "Versuche: ", versuche )
+                    if(errattungDerZahl != zufallszahl):
+                        print("Zahl von dem Computer: ", errattungDerZahl)
+                        nichtErraten = input("Wenn die Zufallszahl ist > als die Zahl von dem computer, schreiben Sie 'nein', wenn die Zufallszahl ist < als die Zahl von dem Computer, schreiben Sie 'ja'")
+                        versuche = int(versuche+1)
+    if(nichtErraten == "ja"):
+        errattungDerZahl = int(errattungDerZahl-(errattungDerZahl/2))
+            if(errattungDerZahl == zufallszahl):
+                gewonnen == True
+                print("Computer hat gewonnen.", "Versuche: ", versuche )
+            if(errattungDerZahl != zufallszahl):
+                print("Zahl von dem Computer: ", errattungDerZahl)
+                nichtErraten = input("Wenn die Zufallszahl ist > als die Zahl von dem computer, schreiben Sie 'nein', wenn die Zufallszahl ist < als die Zahl von dem Computer, schreiben Sie 'ja'")
+                versuche = int(versuche+1)
+        
